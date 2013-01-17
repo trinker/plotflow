7b55ed01e50857ba9e7b952a460105f955bf02f4
tyler rinker <tyler_rinker@hotmail.com>
2013-01-17 14:53:17 -0500

    added ananda's pull request

--- R/mergePDF.R
+++ R/mergePDF.R
@@ -1,17 +1,11 @@
 #helper function
 mergePDF <-
-mergePDF <-
   function(..., file, gsversion = NULL, in.file = NULL) {
     if (is.null(in.file)) {
       in.file <- substitute(...())
     } 
     infiles <- paste(unlist(lapply(in.file, function(y) as.character(y))), 
-<<<<<<< HEAD
         collapse = " ")
-    version <- names(which(Sys.which(c("gs", "gswin32c", "gswin64c")) != ""))
-=======
-                     collapse = " ")
-    
     if (is.null(gsversion)) {
       gsversion <- names(which(Sys.which(c("gs", "gswin32c", "gswin64c")) != ""))
       if (length(gsversion) == 0) 
@@ -21,7 +15,6 @@ mergePDF <-
              paste(gsversion, collapse = " "), 
              ". Please specify which version should be used with the gsversion argument")
     }   
->>>>>>> 5dc7c293e5f0c036d411a8cc419033dcc2ae9d05
     pre = " -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile="
     system(paste(paste(gsversion, pre, file, sep = ""), infiles, collapse = " "))
 }
