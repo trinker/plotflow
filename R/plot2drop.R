3c09bf8e5f083993a9f4f93200438427c4a238b8
tyler rinker <tyler_rinker@hotmail.com>
2013-08-26 22:02:09 -0400

    added plot2drop

---
+++ R/plot2drop.R
@@ -0,0 +1,50 @@
+#' Plot to Drop Box
+#' 
+#' A wrapper to plotting device for easy plotting to Drop Box.
+#' 
+#' @param name The name of the plot (no file extension needed).
+#' @param type The type of plotting device (png, pdf, etc).
+#' @param width Width of plot.
+#' @param height Height of plot.
+#' @param open logical.  If \code{TRUE} provides the code to open the plot in 
+#' default browser.
+#' @param loc The path to the location of the dropbox public folder.
+#' @param key The dropbox key.
+#' @param \ldots Other arguments passed to the plotting device in type.
+#' @details Plots to Drop Box and returns the URL to the plot (and optionally 
+#' opens the plot).
+#' @export
+#' @examples
+#' plot2drop("dfg")
+#' plot(1:10)
+#' dev.off()
+plot2drop <- 
+function(name, type = "png", width = 400, height = 400, 
+    open = TRUE, loc = getOption("dropbox_path"), 
+    key = getOption("dropbox_key"), ...){
+    name <- as.character(substitute(name))
+    if (missing(name)) {
+        stop("must specify a name")
+    }
+    if (is.null(getOption("dropbox_path"))) {
+        stop("must specify a loc (Dropbox Location)")
+    }
+    if (is.null(getOption("dropbox_key"))) {
+        stop("must specify a key (Dropbox Key)")
+    }
+    what <- paste0(name, ".", type)
+    loc <- file.path(loc, what)
+    dev <- match.fun(type)
+    dev(loc, width, height, ...)
+    w <- "dev.off()"
+    x <- paste0("writeClipboard(\"https://dl.dropboxusercontent.com/u/", key,
+        "/", what, "\")")
+    y <- NULL
+    if (open) {
+        y <- paste0("browseURL(\"https://dl.dropboxusercontent.com/u/", key,
+          "/", what, "\")\n")
+    } 
+    z <- paste(w, x, y, sep="\n")
+    writeClipboard(z)
+    message(z)
+}
