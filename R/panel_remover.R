d75e763880a6d1d6809059e0b3600effba30dc7e
tyler rinker <tyler.rinker@gmail.com>
2013-09-28 10:14:18 -0400

    added the more flexible panel_remove

--- R/panel_remover.R
+++ R/panel_remover.R
@@ -9,30 +9,37 @@
 #' If \code{FALSE} the x axis strip.background  is removed.
 #' @return Returns a ggplot2 object with specific strip.background 
 #' removed.
-#' @references \url{http://stackoverflow.com/a/19064688/1000343}
+#' @references \url{http://stackoverflow.com/a/19064621/1000343}
 #' @keywords strip.background
-#' @author Amine Gassem and Tyler Rinker<tyler.rinker@@gmail.com>
+#' @author Roland (stackoverflow.com) and Tyler Rinker<tyler.rinker@@gmail.com>
 #' @export
-#' @importFrom grid editGrob getGrob gpar grid.draw
-#' @importFrom ggplot2 ggplotGrob 
+#' @importFrom grid grob
+#' @importFrom ggplot2 ggplotGrob
 #' @examples
 #' a <- ggplot(mtcars, aes(mpg, hp)) +
 #'   geom_point() +
 #'   facet_grid(cyl~gear) 
 #' 
+#' panel_remover(a)
 #' panel_remover(a, FALSE)
 panel_remover <- function(ggplot_obj, y = TRUE) {
 
-    g <- ggplotGrob(ggplot_obj)
-    gg <- g$grobs
-    loc <- ifelse(y, "right", "top")
-    strip_index <- which(grepl(sprintf("strip-%s", loc), g$layout$name))
-    for(i in strip_index)
-      gg[[i]] <- editGrob(getGrob(gg[[i]], "strip.back",
-          grep=TRUE, global=TRUE), gp = gpar(fill=NA)
-    )
-    
-    g$grobs <- gg
-    grid.draw(g)
+  g <- ggplotGrob(ggplot_obj)
+  what <- ifelse(y, "y", "x")
 
+    g$grobs <- lapply(g$grob, function(gr) {
+        if (any(grepl(paste0("strip.text.", what), names(gr$children)))) {
+            gr$children[[grep("strip.background", names(gr$children))]] <- zeroGrob()
+            gr$children[[grep("strip.text", names(gr$children))]] <- zeroGrob()
+        }
+        return(gr)
+    })
+    class(g) <- unique( c("arrange", "ggplot", class(g), class(ggplot_obj)) )
+    g
+}
+
+zeroGrob <- function() {
+    g0 <- grob(name="NULL")
+    class(g0) <- c("zeroGrob",class(g0))
+    g0
 }
