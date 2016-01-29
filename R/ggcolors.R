c990a3154825ef7fde863517106997d14f033935
Tyler <tyler.rinker@gmail.com>
2016-01-28 20:39:42 -0500

    added ggalig (close #6) fixed ggalign and made own print method (close #7)

---
+++ R/ggcolors.R
@@ -0,0 +1,48 @@
+#' ggplot2's Default Color Scheme
+#' 
+#' Make a palette with \pkg{ggplot2}'s default color scheme.
+#' 
+#' @param n An integer specifying the number of colors.
+#' @return Returns a vector of \code{n} hex colors.
+#' @references \url{http://stackoverflow.com/a/8197703/1000343}
+#' @export
+#' @author John Colby
+#' @examples 
+#' n <- 10
+#' ggcolors(n)
+#'
+#' plot(
+#'     1:n, 
+#'     pch=16, 
+#'     cex=2, 
+#'     col= ggcolors(n)
+#' )
+#' 
+#' barplot(
+#'     stats::setNames(5:14, LETTERS[5:14]), 
+#'     col = ggcolors(n), 
+#'     border = ggcolors(n)
+#' )
+#' 
+#' boxplot(
+#'     x ~ y, 
+#'     data = data.frame(x = rnorm(1000), 
+#'     y = sample(LETTERS[5:14], 1000, TRUE)), 
+#'     border = ggcolors(n),  
+#'     lwd=2
+#' )
+#' 
+#' pie(
+#'     stats::setNames(5:14, LETTERS[5:14]), 
+#'     col = ggcolors(n), 
+#'     border = ggcolors(n)
+#' )
+ggcolors <- function(n) {
+    hues = seq(15, 375, length=n+1)
+    grDevices::hcl(h=hues, l=65, c=100)[1:n]
+}
+
+
+
+
+
