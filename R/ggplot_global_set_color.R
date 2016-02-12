76dcf84aab72b5c01840ff3c53913373cd9e8277
Tyler <tyler.rinker@gmail.com>
2016-02-11 22:10:20 -0500

    updated name of ggplot_col to be more descriptive

--- R/ggplot_global_set_color.R
+++ R/ggplot_global_set_color.R
@@ -1,21 +1,21 @@
-#' Globally Set ggplot2 Colors
+#' Globally Set ggplot2 Colors/Fills
 #' 
 #' Globally set the color and fill of ggplot2.
 #' 
-#' @param col A single color character string.
-#' @param fill logical.  If \code{TRUE} fill is globally altered.
-#' @param colour logical.  If \code{TRUE} colour is globally altered.
+#' @param color A single color character string.
+#' @param alter.fill logical.  If \code{TRUE} fill is globally altered.
+#' @param alter.color logical.  If \code{TRUE} colour is globally altered.
 #' @param reset logical.  If colour and fill are reset to black before applying 
 #' new color.
 #' @references \url{http://stackoverflow.com/a/21175042/1000343}
 #' @export
 #' @seealso \code{\link[ggplot2]{update_geom_defaults}}
 #' @examples
-#' ggplot_col("purple")
+#' ggplot_global_set_color("purple")
 #' 
 #' ggplot(mtcars, aes(factor(cyl))) + geom_bar()
 #' ggplot(mtcars, aes(hp, mpg, group = factor(cyl))) + geom_point()
-ggplot_col <- function(col = "black", fill = TRUE, colour = TRUE, reset = TRUE) {
+ggplot_global_set_color <- function(color = "black", alter.fill = TRUE, alter.color = TRUE, reset = TRUE) {
     
     params <- ls(pattern = 'Geom[A-Z]', envir = as.environment('package:ggplot2'))
     geoms <- gsub("Geom", "", params)
@@ -26,15 +26,15 @@ ggplot_col <- function(col = "black", fill = TRUE, colour = TRUE, reset = TRUE) 
         invisible(lapply(geoms, ggplot2::update_geom_defaults, list(fill = "black", 
         	colour = "black")))
     }
-    if (colour && !fill) {
-        invisible(lapply(geoms, ggplot2::update_geom_defaults, list(colour = col)))
+    if (alter.color && !alter.fill) {
+        invisible(lapply(geoms, ggplot2::update_geom_defaults, list(colour = color)))
     } else {
-        if (colour && fill) {
-            invisible(lapply(geoms, ggplot2::update_geom_defaults, list(fill = col, 
-            	colour = col)))
+        if (alter.color && alter.fill) {
+            invisible(lapply(geoms, ggplot2::update_geom_defaults, list(fill = color, 
+            	colour = color)))
         } else {
-            if (!colour && fill) {
-                invisible(lapply(geoms, ggplot2::update_geom_defaults, list(fill = col)))
+            if (!alter.color && alter.fill) {
+                invisible(lapply(geoms, ggplot2::update_geom_defaults, list(fill = color)))
             } else {
                 stop("better rethink why you're using this function")
             }
